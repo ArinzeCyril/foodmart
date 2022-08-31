e268d72cca59769426fa54043053a7e6b10403f0
Arinze Cyril <61941210+ArinzeCyril@users.noreply.github.com>
2022-08-31 21:22:46 +0100

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// mobile nav display
+const primaryNav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible")
+   
+const setTrue = () => {
+    primaryNav.setAttribute("data-visible", true)
+    navToggle.setAttribute("area-expanded", true)
+}
+
+const setFalse = () => {
+    primaryNav.setAttribute('data-visible', false)
+    navToggle.setAttribute("area-expanded", false)
+}
+
+visibility === "false" ? setTrue() : setFalse();
+})
