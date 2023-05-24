5a2ab5fc627b5a9f9c056b1a62439b3a632b648e
Arinze Cyril <61941210+ArinzeCyril@users.noreply.github.com>
2023-05-24 23:50:10 +0100

    pages upgrade

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 // mobile nav display
+const container = document.querySelector('.container')
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute("data-visible")
    
-const setTrue = () => {
-    primaryNav.setAttribute("data-visible", true)
-    navToggle.setAttribute("area-expanded", true)
-}
+    const setTrue = () => {
+        primaryNav.setAttribute("data-visible", true)
+        navToggle.setAttribute("area-expanded", true)
+    }
 
-const setFalse = () => {
-    primaryNav.setAttribute('data-visible', false)
-    navToggle.setAttribute("area-expanded", false)
-}
+    const setFalse = () => {
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute("area-expanded", false)
+    }
 
-visibility === "false" ? setTrue() : setFalse();
+    visibility === "false" ? setTrue() : setFalse();
+})
+
+container.addEventListener('click', () => {
+    const setFalse = () => {
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute("area-expanded", false)
+    }
+
+    primaryNav && setFalse()
+    console.log('cont clicked');
 })
